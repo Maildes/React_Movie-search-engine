5676d68f8e507100beb6af150409e7d3a37a1680
Maildes <vlad.mailyk8@gmailcom>
2020-09-22 22:48:35 +0300

    Updates

---
+++ precache-manifest.3bee0dfff62f976e526f722e179b0d41.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243e0db7ccec29538a405be9ae9af2ad",
+    "url": "/React_Movie-search-engine/index.html"
+  },
+  {
+    "revision": "ea690bdf07162062d98b",
+    "url": "/React_Movie-search-engine/static/css/main.66084582.chunk.css"
+  },
+  {
+    "revision": "d19e7784c3b765d02f92",
+    "url": "/React_Movie-search-engine/static/js/2.b7905fc4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Movie-search-engine/static/js/2.b7905fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea690bdf07162062d98b",
+    "url": "/React_Movie-search-engine/static/js/main.e4933a15.chunk.js"
+  },
+  {
+    "revision": "3b7d28d2e4fc595bcd9b",
+    "url": "/React_Movie-search-engine/static/js/runtime-main.c0ab5b43.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "/React_Movie-search-engine/static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "/React_Movie-search-engine/static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "/React_Movie-search-engine/static/media/Mont-SemiBold.d41a520b.otf"
+  },
+  {
+    "revision": "77d24b85736e225456bd732bc4abe613",
+    "url": "/React_Movie-search-engine/static/media/double-angle-pointing-to-right.77d24b85.svg"
+  },
+  {
+    "revision": "427f7f4e9ff383c0c3fe24d31295afd0",
+    "url": "/React_Movie-search-engine/static/media/double-left-chevron.427f7f4e.svg"
+  },
+  {
+    "revision": "79fb752b6ed527c9ab5a136bf4b704d8",
+    "url": "/React_Movie-search-engine/static/media/left.79fb752b.svg"
+  },
+  {
+    "revision": "d38bc38ad9ba60f9091aa2a9b3f4190f",
+    "url": "/React_Movie-search-engine/static/media/poster_none.d38bc38a.png"
+  },
+  {
+    "revision": "b9aef880b29af6667ceb0757f8934368",
+    "url": "/React_Movie-search-engine/static/media/rating-star.b9aef880.svg"
+  },
+  {
+    "revision": "6be8962252147b18cadfcff5b3b54611",
+    "url": "/React_Movie-search-engine/static/media/right.6be89622.svg"
+  }
+]);
--- src/helpers/HelloMarakas.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const HelloMarakas = () => (
-  <div className="hello__marakas">
-    <img src="https://marakasdesign.com/wp-content/themes/semplice/images/eye.gif" alt="eye"/>
-  </div>
-)
